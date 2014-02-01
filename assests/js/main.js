76f7146ccf7a0643f16d32ad17ec45eeb5ea7994
caleb <caleb@summitprojects.com>
2014-01-31 18:18:25 -0800

    Blog Section framed and almost complete. Design changes / small edits are what I'm awaiting

--- assests/js/main.js
+++ assests/js/main.js
@@ -15,6 +15,8 @@ $(document).ready(function() {
 	});
 
 
+	cancelZoom();
+
 
 
 
@@ -71,6 +73,46 @@ function scrollFn() {
 
 
 
+
+//---------------------------------------------------------------------------| Cancel Zoom on Mobile Inputs
+function cancelZoom() {
+    var d = document,
+        viewport,
+        content,
+        maxScale = ',maximum-scale=',
+        maxScaleRegex = /,*maximum\-scale\=\d*\.*\d*/;
+ 
+    // this should be a focusable DOM Element
+    if(!this.addEventListener || !d.querySelector) {
+        return;
+    }
+ 
+    viewport = d.querySelector('meta[name="viewport"]');
+    content = viewport.content;
+ 
+    function changeViewport(event)
+    {
+        // http://nerd.vasilis.nl/prevent-ios-from-zooming-onfocus/
+        viewport.content = content + (event.type == 'blur' ? (content.match(maxScaleRegex, '') ? '' : maxScale + 10) : maxScale + 1);
+    }
+ 
+    // We could use DOMFocusIn here, but it's deprecated.
+    this.addEventListener('focus', changeViewport, true);
+    this.addEventListener('blur', changeViewport, false);
+}
+ 
+// jQuery-plugin
+(function($)
+{
+    $.fn.cancelZoom = function()
+    {
+        return this.each(cancelZoom);
+    };
+ 
+    // Usage:
+    $('input:text,select,textarea').cancelZoom();
+})(jQuery);
+//---------------------------------------------------------------------------| Cancel Zoom on Mobile Inputs
 
 
 
